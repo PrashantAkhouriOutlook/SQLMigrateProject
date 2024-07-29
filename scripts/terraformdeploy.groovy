63878af0b735cd0e4e9e5ebd695ed71b11be1317
prashant <prakhour@outlook.com>
2024-07-29 18:47:45 +0530

    test run

--- scripts/terraformdeploy.groovy
+++ scripts/terraformdeploy.groovy
@@ -2,8 +2,15 @@
 def terraformDeploy() {
 
      sh "pwd"
-
-
+     dir("${env.WORKSPACE}/aQA"){
+    sh "pwd"
+      }
+      dir("${env.WORKSPACE}/aQA") {
+                    // Change to the specified directory and run Terraform commands
+                    sh 'terraform init'
+                    sh 'terraform plan'
+                    sh 'terraform apply -auto-approve'
+                }
     }
 
 terraformDeploy()
